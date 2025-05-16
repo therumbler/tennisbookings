2204ba3e71dfa6826e53cc8d173193a27a920455
Benjamin Rumble <therumbler@gmail.com>
2025-05-16 10:18:47 -0400

    use datetime.now() in fetch_all_courts()

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
-import json
+from datetime import datetime
+import logging
+
 from lib.courtreserve import fetch_courts
 
 
 def fetch_all_courts():
+    """fetch courts from all systems"""
+    logging.basicConfig(level="DEBUG")
     org_id = 10243  # McCarren Park
-    start_date = "2024-11-07"
+    start_date = datetime.now().strftime("%Y-%m-%d")
+
     court_reserve_resp = fetch_courts(org_id, start_date)
-    print(json.dumps(court_reserve_resp))
+
+    return court_reserve_resp
 
 
 def main():
-    fetch_all_courts()
+    """let's kick it all off"""
+    resp = fetch_all_courts()
+    print(resp["Court #4"])
 
 
 if __name__ == "__main__":
