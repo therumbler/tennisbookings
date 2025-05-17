5e9ea2c56b6d9fb3e1b5d748a2e403919b8705d2
Benjamin Rumble <therumbler@gmail.com>
2025-05-17 12:16:30 -0400

    add nycgovparks.py

---
+++ lib/nycgovparks.py
@@ -0,0 +1,126 @@
+"""fetch tennis info from nycgovparks.gov"""
+
+from datetime import datetime, timedelta
+import re
+import logging
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+
+
+from models.time_slot import TimeSlot
+
+logger = logging.getLogger(__name__)
+
+
+def _fetch_html(court_id):
+    url = f"https://www.nycgovparks.org/tennisreservation/availability/{court_id}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+        "accept": "text/html",
+    }
+    req = Request(url, headers=headers)
+    logger.info("fetching %s", url)
+    with urlopen(req) as resp:
+        return resp.read().decode()
+
+
+def _get_rows_from_div(div):
+    rows_pattern = r"<tr>.*?</tr>"
+    rows = re.findall(rows_pattern, div)
+    return rows
+
+
+def _get_time_from_row(row):
+    time_string = _get_time_string_from_row(row)
+
+
+def _get_time_string_from_row(row):
+    time_pattern = r"<strong>(.*?)</strong>"
+    time = None
+    try:
+        time = re.search(time_pattern, row).group(1)
+    except AttributeError:
+        logger.warning("row %s does not have a time", row)
+
+    return time
+
+
+def _time_string_to_datetime(time_string, date):
+    # create a datetime object from date (yyyy-mm-dd) and time (h:mm p.m/a.m) variables
+    dt_str = None
+    try:
+        dt_str = f"{date} {time_string.replace('.', '').upper()}"
+        dt = datetime.strptime(dt_str, "%Y-%m-%d %I:%M %p")
+    except Exception as e:
+        logger.warning(
+            "Could not parse datetime from date '%s' and time '%s': %s",
+            date,
+            time_string,
+            e,
+        )
+    return dt_str
+
+
+def _get_timeslot_from_row(row, date, court_name):
+    # logger.debug("row %s", row)
+    time_string = _get_time_string_from_row(row)
+    dt = _time_string_to_datetime(time_string, date)
+
+    # logger.info("dt: %s", dt)
+    if "status2" in row:
+        is_booked = False
+    else:
+        is_booked = True
+
+    return TimeSlot(datetime_str=str(dt), is_booked=is_booked, court_name=court_name)
+
+
+def _div_to_timeslots(div):
+    date = re.search(r"\d{4}-\d{2}-\d{2}", div).group()
+    rows = _get_rows_from_div(div)
+    time_slots = []
+    court_name = ""
+    for row in rows:
+        if "Court" in row:
+            court_name = re.search(r"Court \d", row).group()
+        else:
+            time_slots.append(_get_timeslot_from_row(row, date, court_name))
+    time_slots = [ts for ts in time_slots if ts]
+    logger.info("found %d timeslots on %s", len(time_slots), date)
+    return time_slots
+
+
+def _get_timeslots_from_html(html):
+    pattern = r'<div id="\d{4}-\d{2}-\d{2}".*?</div>'
+    matches = re.findall(pattern, html, re.DOTALL)
+
+    if not matches:
+        logger.error("no matches found in html")
+    timeslots: list[TimeSlot] = []
+    for div in matches:
+        timeslots.extend(_div_to_timeslots(div))
+
+    return timeslots
+
+
+def _fetch_availability(court_id):
+    html = _fetch_html(court_id)
+
+    return _get_timeslots_from_html(html)
+
+
+def fetch_courts(court_id):
+    resp = _fetch_availability(court_id)
+    return resp
+
+
+def main():
+    logging.basicConfig(level="DEBUG")
+    court_id = 11  # McCarren Park
+
+    resp = fetch_courts(court_id)
+    # print(resp)
+
+
+if __name__ == "__main__":
+    main()
--- models/time_slot.py
+++ models/time_slot.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, asdict
 
 @dataclass
 class TimeSlot:
+    court_name: str
     datetime_str: str
     is_booked: bool = False
 
