2b3b431fc9b464a3d06906073d242adc9c32e644
Benjamin Rumble <therumbler@gmail.com>
2025-05-23 12:36:20 -0400

    add email.py add settings.py

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import logging
 
 # from lib.courtreserve import fetch_courts
 from lib.nycgovparks import fetch_available_courts
+from lib.email import send_email
+
+import settings
+
+logger = logging.getLogger(__name__)
 
 
-def fetch_all_courts():
+def fetch_all_available_courts():
     """fetch courts from all systems"""
     logging.basicConfig(level="INFO")
     # org_id = 10243  # McCarren Park
@@ -18,9 +23,57 @@ def fetch_all_courts():
     return nyc_gov_parks_resp
 
 
+def _timeslot_to_str(timeslot):
+    """convert timeslot to string"""
+    return f"{timeslot.court_name}|{timeslot.datetime_str}"
+
+
+def _check_timeslot_if_notified(timeslot) -> bool:
+    """check if the timeslot has been notified"""
+    # check the notified.txt file for timeslot court_name and datetime_str
+    # if found, return True
+    # if not found, return False
+    try:
+        with open("notified.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                if _timeslot_to_str(timeslot) in line:
+                    return True
+    except FileNotFoundError:
+        pass
+    return False
+
+
+def _should_notify(resp) -> bool:
+    should_notify = False
+    for timeslot in resp:
+        if not _check_timeslot_if_notified(timeslot):
+            should_notify = True
+
+    return should_notify
+
+
+def _notify(resp):
+    body = "The following courts are available:\n"
+    body += "\n".join([_timeslot_to_str(timeslot) for timeslot in resp])
+    send_email(
+        subject="NYC Parks Tennis Court Availability",
+        body=body,
+        to_email=settings.TO_EMAIL,
+        from_email=settings.FROM_EMAIL,
+        smtp_port=settings.SMTP_PORT,
+        smtp_server=settings.SMTP_SERVER,
+        username=settings.SMTP_USERNAME,
+        password=settings.SMTP_PASSWORD,
+    )
+
+
 def main():
     """let's kick it all off"""
-    resp = fetch_all_courts()
+    resp = fetch_all_available_courts()
+    if _should_notify(resp):
+        logger.info("notifying")
+        _notify(resp)
     print(resp)
 
 
---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+SMTP_SERVER = os.environ["SMTP_SERVER"]
+SMTP_PORT = os.environ["SMTP_PORT"]
+SMTP_USERNAME = os.environ["SMTP_USERNAME"]
+SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
+FROM_EMAIL = os.environ["FROM_EMAIL"]
+TO_EMAIL = os.environ["TO_EMAIL"]
