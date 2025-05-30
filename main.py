978b27e0e1ca73cb26052c923f88f8e87cb1fe47
Benjamin Rumble <therumbler@gmail.com>
2025-05-29 21:41:13 -0400

    add email notifications (#3)
    * add notify.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-from datetime import datetime
 import logging
 
 # from lib.courtreserve import fetch_courts
 from lib.nycgovparks import fetch_available_courts
+from lib.notify import send_email
+
+import settings
+
+logger = logging.getLogger(__name__)
+
+NOTIFIED_TEXT_FILE_PATH = "notified.txt"
 
 
-def fetch_all_courts():
+def fetch_all_available_courts():
     """fetch courts from all systems"""
     logging.basicConfig(level="INFO")
     # org_id = 10243  # McCarren Park
@@ -18,10 +24,68 @@ def fetch_all_courts():
     return nyc_gov_parks_resp
 
 
+def _timeslot_to_str(timeslot):
+    """convert timeslot to string"""
+    return f"{timeslot.court_name}|{timeslot.datetime_str}"
+
+
+def _check_timeslot_if_notified(timeslot) -> bool:
+    """check if the timeslot has been notified"""
+    try:
+        with open(NOTIFIED_TEXT_FILE_PATH, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                if _timeslot_to_str(timeslot) in line:
+                    return True
+    except FileNotFoundError:
+        pass
+    return False
+
+
+def _mark_timeslot_as_notified(timeslot):
+    """mark the timeslot as notified"""
+    with open(NOTIFIED_TEXT_FILE_PATH, "a") as f:
+        f.write(_timeslot_to_str(timeslot) + "\n")
+    logger.info(f"marked {timeslot.court_name} at {timeslot.datetime_str} as notified")
+
+
+def _should_notify(resp) -> bool:
+    should_notify = False
+    for timeslot in resp:
+        if not _check_timeslot_if_notified(timeslot):
+            should_notify = True
+
+    return should_notify
+
+
+def _notify(resp):
+    body = "The following courts are available:\n"
+    body += "\n".join([_timeslot_to_str(timeslot) for timeslot in resp])
+    return send_email(
+        subject="NYC Parks Tennis Court Availability",
+        body=body,
+        to_email=settings.TO_EMAIL,
+        from_email=settings.FROM_EMAIL,
+        smtp_port=settings.SMTP_PORT,
+        smtp_server=settings.SMTP_SERVER,
+        username=settings.SMTP_USERNAME,
+        password=settings.SMTP_PASSWORD,
+    )
+
+
 def main():
     """let's kick it all off"""
-    resp = fetch_all_courts()
+    resp = fetch_all_available_courts()
     print(resp)
+
+    if not _should_notify(resp):
+        logger.info("no new timeslots available, not notifying")
+        return
+    logger.info("notifying")
+    if _notify(resp):
+        logger.info("notification sent")
+        for timeslot in resp:
+            _mark_timeslot_as_notified(timeslot)
 
 
 if __name__ == "__main__":
