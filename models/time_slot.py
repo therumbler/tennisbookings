89489c7e9c9ac30a6fe6f7fc55f60f744449d86b
Benjamin Rumble <therumbler@gmail.com>
2025-06-17 14:51:00 -0400

    add key value to TimeSlot dataclass

--- models/time_slot.py
+++ models/time_slot.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass, asdict
 from datetime import datetime
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -10,6 +14,10 @@ class TimeSlot:
     datetime_obj: datetime
     booking_url: str
     is_booked: bool = False
+    key: str = None
+
+    def __post_init__(self):
+        self.key = f"{self.location_name}-{self.court_name}-{self.datetime_str}"
 
     def asdict(self):
         return asdict(self)
